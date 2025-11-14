6f83b0518c940f6f9c5905637f63b04c2c5e5b53
Jenny Peng <jejpeng@Jennys-MacBook-Pro.local>
2025-11-13 19:33:15 -0600

    directory

---
+++ terraform-jenkins/backend.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.0"
+  cloud {
+    organization = "eren-team"
+    workspaces {
+      name = "jenkins-prod"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
